459a1b0723e4250c653f4a34226263d3d3655983
Pati204 <patrysia18122000@wp.pl>
2022-07-10 21:10:05 +0200

    add 3 new xaml views

---
+++ HomePage.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace ApplicationRestaurant
+{
+    /// <summary>
+    /// Interaction logic for HomePage.xaml
+    /// </summary>
+    public partial class HomePage : Window
+    {
+        private bool _showLunch;
+        public HomePage()
+        {
+            InitializeComponent();
+        }
+
+        private void click_sniadanie(object sender, RoutedEventArgs e)
+        {
+            Application.Current.MainWindow = new MainWindow();
+            Application.Current.MainWindow.Show();
+            this.Close();
+        }
+
+        private void LunchAction(object sender, RoutedEventArgs e)
+        {
+            this.toggleStackPanel(LunchPanel);
+        }
+
+        private void DrinkAction(object sender, RoutedEventArgs e)
+        {
+            this.toggleStackPanel(DrinkPanel);
+        }
+
+        private void toggleStackPanel(StackPanel element)
+        {
+            LunchPanel.Visibility = Visibility.Hidden;
+            DrinkPanel.Visibility = Visibility.Hidden;
+
+            element.Visibility = Visibility.Visible;
+            
+        }
+    }
+}
--- Register.xaml.cs
+++ Register.xaml.cs
@@ -10,27 +10,18 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace ApplicationRestaurant
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Interaction logic for Register.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class Register : Window
     {
-        public MainWindow()
+        public Register()
         {
             InitializeComponent();
-        }
-
-        private void LoginAction(object sender, RoutedEventArgs e)
-        {
-            
-            Application.Current.MainWindow = new HomePage();
-            Application.Current.MainWindow.Show();
-            this.Close();
         }
     }
 }
